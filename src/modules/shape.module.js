968e975a58516796474c93f15e61037b558b94b8
bleak <ffeeaarrofdeath@mail.ru>
2024-05-19 13:15:29 +0300

    delete logs

--- src/modules/shape.module.js
+++ src/modules/shape.module.js
@@ -9,24 +9,19 @@ export class ShapeModule extends Module {
 
     const randomFigureHeight = Math.floor(Math.random() * 600);
     figure.style.height = `${randomFigureHeight}px`;
-    console.log(randomFigureHeight);
 
     const randomFigureWidth = Math.floor(Math.random() * 800);
     figure.style.width = `${randomFigureWidth}px`;
-    console.log(randomFigureWidth);
 
     const marginFromTop = Math.floor(Math.random() * 400);
     figure.style.marginTop = `${marginFromTop}px`;
-    console.log(marginFromTop);
 
     const marginFromLeft = Math.floor(Math.random() * 1000);
     figure.style.marginLeft = `${marginFromLeft}px`;
-    console.log(marginFromLeft);
 
     const randomBorder = Math.floor(Math.random() * 100);
     figure.style.borderRadius = `${randomBorder}px`;
-    console.log(randomBorder);
 
-    document.body.append(figure);
+    document.body.appendChild(figure);
   }
 }
